25bfbf4a58bc4ddca645d24d654289052dad23a0
Javier Arias <javier@arias.re>
2018-07-13 14:47:34 +0100

    add primary keys; capitalise keywords

--- schema/10-schema.sql
+++ schema/10-schema.sql
@@ -1,31 +1,31 @@
 
-create table country (
-  country_name text not null,
-  country_code char(2) unique not null
+CREATE TABLE country (
+  country_code char(2) PRIMARY KEY NOT NULL,
+  country_name text NOT NULL
 );
 
-create table institution (
-  institution_uuid uuid unique not null,
-  institution_name text not null,
-  institution_country_code char(2) not null references country(country_code)
+CREATE TABLE institution (
+  institution_uuid uuid PRIMARY KEY NOT NULL,
+  institution_name text NOT NULL,
+  institution_country_code char(2) NOT NULL REFERENCES country(country_code)
 );
 
-create table ip_range (
-  ip_range_value cidr unique not null,
-  institution_uuid uuid not null references institution(institution_uuid)
+CREATE TABLE ip_range (
+  ip_range_value cidr PRIMARY KEY NOT NULL,
+  institution_uuid uuid NOT NULL REFERENCES institution(institution_uuid)
 );
 
-create table contact (
-  contact_name text not null,
-  contact_email_address text not null,
-  contact_notes text,
-  institution_uuid uuid not null references institution(institution_uuid)
+CREATE TABLE contact (
+  contact_name text PRIMARY KEY NOT NULL,
+  contact_email_address text NULL,
+  contact_notes text NULL,
+  institution_uuid uuid NOT NULL REFERENCES institution(institution_uuid)
 );
 
-create table institution_relation (
-  ir_parent_id uuid not null references institution(institution_uuid),
-  ir_child_id uuid not null references institution(institution_uuid),
-  unique (ir_parent_id, ir_child_id),
-  constraint no_self_referencing check (ir_parent_id <> ir_child_id)
+CREATE TABLE institution_relation (
+  ir_parent_id uuid NOT NULL REFERENCES institution(institution_uuid),
+  ir_child_id uuid NOT NULL REFERENCES institution(institution_uuid),
+  PRIMARY KEY (ir_parent_id, ir_child_id),
+  CONSTRAINT no_self_referencing CHECK (ir_parent_id <> ir_child_id)
 );
 
